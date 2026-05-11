71ab6df52332379aa88af2ed20aeb50ab7670b53
tlipinski <git@t3xh.nl>
2026-05-11 22:30:52 +0200

    refactor: replace Cargo version with Git-derived version

---
+++ build.rs
@@ -0,0 +1,34 @@
+use std::process::Command;
+
+fn main() {
+    println!("cargo:rustc-env=APP_VERSION={}", get_git_version());
+    println!("cargo:rerun-if-changed=.git/HEAD");
+    println!("cargo:rerun-if-changed=.git/index");
+}
+
+fn get_git_version() -> String {
+    let tag = git_output(&["describe", "--tags", "--exact-match"]);
+
+    tag.unwrap_or(
+        {
+            match (
+                git_output(&["rev-list", "--count", "HEAD"]),
+                git_output(&["rev-parse", "--short=7", "HEAD"]),
+            ) {
+                (Some(rev_count), Some(short_hash)) => format!("r{rev_count}.{short_hash}"),
+                _ => env!("CARGO_PKG_VERSION").to_string(),
+            }
+        }
+    )
+}
+
+fn git_output(args: &[&str]) -> Option<String> {
+    let output = Command::new("git").args(args).output().ok()?;
+    if !output.status.success() {
+        return None;
+    }
+
+    String::from_utf8(output.stdout)
+        .ok()
+        .map(|v| v.trim().to_owned())
+}
